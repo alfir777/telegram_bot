d13408949a0728bcdb1f0b9b7f6ca25aa6dd9478
Alfir Tyakin <alfir777@rambler.ru>
2021-08-03 15:38:15 +0500

    add logging

--- echo_bot.py
+++ echo_bot.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import json
+import logging
 import os
 import types
 
@@ -16,9 +17,26 @@ name = ''
 surname = ''
 age = 0
 
+log = logging.getLogger('bot')
+
+
+def configure_logging():
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(logging.Formatter("%(levelname)s %(message)s"))
+    stream_handler.setLevel(logging.INFO)
+    log.addHandler(stream_handler)
+
+    file_handler = logging.FileHandler('log/echo_bot.log')
+    file_handler.setFormatter(logging.Formatter("%(asctime)s %(levelname)s %(message)s"))
+    file_handler.setLevel(logging.DEBUG)
+    log.addHandler(file_handler)
+
+    log.setLevel(logging.DEBUG)
+
 
 @bot.message_handler(content_types=['text'])
 def start(message):
+    log.debug(f'{message}')
     if message.text == "/reg":
         bot.send_message(message.from_user.id, 'Как тебя зовут?')
         bot.register_next_step_handler(message, get_name)
@@ -58,11 +76,15 @@ def get_age(message):
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
-    if call.data == "yes":
-        bot.send_message(call.message.chat.id, 'Запомню : )')
-    elif call.data == "no":
-        pass
+    try:
+        if call.data == "yes":
+            bot.send_message(call.message.chat.id, 'Запомню : )')
+        elif call.data == "no":
+            start(call.message)
+    except Exception as exc:
+        log.exception(f'нажатие на NO выдало {exc}')
 
 
 if __name__ == '__main__':
+    configure_logging()
     bot.polling(none_stop=True, interval=0)
